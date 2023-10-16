e333d8b4e19de3a6f7f94c27bae0265e8c6e8823
Zhongbin Hu <zh1323@ic.ac.uk>
2023-10-16 14:37:04 +0100

    move data

---
+++ week3/code/notes.R
@@ -0,0 +1,26 @@
+> v <- c(1,2,3,4,5)
+> prod(v+1)
+[1] 720
+> wing.width <- 1.2
+> wing.length <- c(4.7, 5.2)
+> wing.
+Error: object 'wing.' not found
+> wing.
+wing.length  wing.width   
+> wing.
+Error: object 'wing.' not found
+> x <- (1 + (2 * 3)
++ 
+> a <- T
+> a
+[1] TRUE
+> class(a)
+[1] "logical"
+> mat1 <- matrix(1:25, 5,5)
+> print(mat1)
+     [,1] [,2] [,3] [,4] [,5]
+[1,]    1    6   11   16   21
+[2,]    2    7   12   17   22
+[3,]    3    8   13   18   23
+[4,]    4    9   14   19   24
+[5,]    5   10   15   20   25
