d36051ec0bf4bc00efa41b1fb5f65ff4cf842b63
Zhongbin Hu <zh1323@ic.ac.uk>
2023-11-03 16:20:33 +0000

    week3

--- week3/code/DataWrangTidy.R
+++ week3/code/DataWrangTidy.R
@@ -10,11 +10,10 @@ MyData <- as.data.frame(t(MyData))
 MyData[MyData == ""] <- 0
 
 #convert from wide to long format
-MyData <- MyData[-1,] #remove the first row
-colnames(MyData) <- MyData[1,] #set column names from the original data
-MyData <- MyData[-1,] #remove the second row
+TempData <- MyData[-1,] #remove the first row
+colnames(TempData) <- MyData[1,] #set column names from the original data
 
-MyWrangledData <- MyData %>%
+MyWrangledData <- TempData%>%
   gather(key = "Species", value = "Count", -Cultivation, -Block, -Plot, -Quadrat) %>%
   mutate(
     Cultivation = as.factor(Cultivation),
