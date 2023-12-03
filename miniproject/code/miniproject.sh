50b4825dcf2a48e672b3564bd94e5591a48bf255
Lakezh <146999369+Lakezh@users.noreply.github.com>
2023-12-03 00:07:04 +0000

    Update miniproject.sh

--- miniproject/code/miniproject.sh
+++ miniproject/code/miniproject.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-
+if [ -f "miniproject.R" ]; then
+   echo "Running miniproject.R"
+   python3 Modified.py
+else
+   echo "File Not Found: miniproject.R does not exist."
+   exit 1
+fi
 Rscript miniproject.R
 
 pdflatex miniproject.tex
