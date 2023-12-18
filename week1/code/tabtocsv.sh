ee69ee7e51ad0584f381b40079cb83e93f0a1bf7
Lakezh <146999369+Lakezh@users.noreply.github.com>
2023-12-18 11:26:07 +0000

    Update tabtocsv.sh

--- week1/code/tabtocsv.sh
+++ week1/code/tabtocsv.sh
@@ -1,3 +1,12 @@
+#!/bin/sh
+# Author: zh1323@ic.ac.uk
+# Script: tabtocsv.sh
+# Description: substitute the tabs in the files with commas
+#
+# Saves the output into a .csv file
+# Arguments: 1 -> tab delimited file
+# Date: Oct 2023
+
 if [ $# -ne 1 ]; then
 #when the input files are not 1, the message comes out using echo
     echo "Error. Format: $0 <InputFile1>"
