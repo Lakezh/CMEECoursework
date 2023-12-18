81790f516bf55dc60e61a5e540c6a5efa1d51399
Lakezh <146999369+Lakezh@users.noreply.github.com>
2023-12-18 11:30:17 +0000

    Update boilerplate.py

--- week2/code/boilerplate.py
+++ week2/code/boilerplate.py
@@ -1,12 +1,12 @@
-__appname__= '[application name here]'
-__author__ = 'your name (e-mail adress)'
+__appname__= '[boilerplate]'
+__author__ = 'Zhongbin Hu (zh1323@ic.ac.uk)'
 __version__ = '0.0.1'
 __license__ = "Licese for this code/program"
 
 import sys
 
 def main(argv):
-    print('This is a boiilerplate')
+    print('This is a boilerplate')
     return 0
 
 if __name__ == "__main__":
